40751bf1bfa38a6b1a6572ac3b9dfea8543a6ff4
jiro4989 <jiroron666@gmail.com>
2019-01-02 01:47:27 +0900

    :sparkles:多角形を生成できるようになった

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"math"
+	"os"
 
 	svg "github.com/ajstarks/svgo"
 )
@@ -19,16 +19,14 @@ func main() {
 	if err := json.Unmarshal(b, &classes); err != nil {
 		panic(err)
 	}
-	for _, c := range classes {
-		// fmt.Println(c.Name)
-		// fmt.Println(c.Params)
-		// WriteSVG(os.Stdout, c)
-
+	for i, c := range classes {
+		if i == 0 {
+			// Classes.jsonの最初のデータは絶対にnullのためスキップ
+			continue
+		}
 		r, w, h := 50, 100, 100
 		xs, ys := PolygonXYs(c, r, w, h)
-		fmt.Println("x:", xs)
-		fmt.Println("y:", ys)
-		fmt.Println("------------")
+		WriteSVG(os.Stdout, c, xs, ys)
 	}
 }
 
@@ -43,13 +41,28 @@ func PolygonXYs(c Class, r, w, h int) (xs []int, ys []int) {
 		n := float64(360 / len(c.Params) * i)
 		x := fr*math.Cos(n*math.Pi/180) + cx
 		y := fr*math.Sin(n*math.Pi/180) + cy
+
+		max := 255
+		switch i {
+		case 0: // MHP
+			max = 9999
+		case 1: // MMP
+			max = 2000
+		case 6, 7: // AGI, LUK
+			max = 500
+		}
+		p := c.Params[i]
+		last := p[len(p)-1]
+		x = x * float64(last) / float64(max)
+		y = y * float64(last) / float64(max)
+
 		xs = append(xs, int(x))
 		ys = append(ys, int(y))
 	}
 	return
 }
 
-func WriteSVG(w io.Writer, c Class) {
+func WriteSVG(w io.Writer, c Class, xs, ys []int) {
 	var (
 		width  = 100
 		height = 100
@@ -58,6 +71,7 @@ func WriteSVG(w io.Writer, c Class) {
 	canvas := svg.New(w)
 	canvas.Start(width, height)
 	canvas.Circle(width/2, height/2, 100)
+	canvas.Polygon(xs, ys)
 	canvas.Text(width/2, height/2, title, "text-anchor:middle;font-size:30px;fill:white")
 	canvas.End()
 }
