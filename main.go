a005c175cc455c478fdb9fdaa3a5423f55da7d2f
jiro4989 <jiroron666@gmail.com>
2019-01-02 01:11:11 +0900

    :sparkles:とりあえず座標は出せた

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
-	"os"
+	"math"
 
 	svg "github.com/ajstarks/svgo"
 )
@@ -20,10 +20,33 @@ func main() {
 		panic(err)
 	}
 	for _, c := range classes {
-		fmt.Println(c.Name)
-		fmt.Println(c.Params)
-		WriteSVG(os.Stdout, c)
+		// fmt.Println(c.Name)
+		// fmt.Println(c.Params)
+		// WriteSVG(os.Stdout, c)
+
+		r, w, h := 50, 100, 100
+		xs, ys := PolygonXYs(c, r, w, h)
+		fmt.Println("x:", xs)
+		fmt.Println("y:", ys)
+		fmt.Println("------------")
 	}
+}
+
+// PolygonXYs はClassのパラメータからX,Y座標のスライスを返す
+func PolygonXYs(c Class, r, w, h int) (xs []int, ys []int) {
+	var (
+		cx = float64(w / 2) // 中心x座標
+		cy = float64(h / 2) // 中心y座標
+		fr = float64(r)     // sin/cos計算のためにfloatに型を合わせるため
+	)
+	for i := 0; i < len(c.Params); i++ {
+		n := float64(360 / len(c.Params) * i)
+		x := fr*math.Cos(n*math.Pi/180) + cx
+		y := fr*math.Sin(n*math.Pi/180) + cy
+		xs = append(xs, int(x))
+		ys = append(ys, int(y))
+	}
+	return
 }
 
 func WriteSVG(w io.Writer, c Class) {
