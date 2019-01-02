617fbe37cde924c0decac9e9466b4bd1645f5743
jiro4989 <jiroron666@gmail.com>
2019-01-03 02:50:46 +0900

    :sparkles:リファクタした

--- main.go
+++ main.go
@@ -35,12 +35,14 @@ func main() {
 		}
 		var (
 			r    = 250.0
+			pr   = r - 25.0
 			w, h = r * 2, r * 2
 			cp   = point.Point{X: w / 2, Y: h / 2}
 		)
-		paramPP := PolygonPoint(c.Params, r, cp)
-		titlePP := point.RegularPolygonPoint(r, w, h, 8)
-		WriteSVG2(os.Stdout, w, h, paramPP, titlePP)
+		paramPP := PolygonPoint(c.Params, pr, cp)
+		titlePP := point.RegularPolygonPoint(pr, w, h, 8)
+		textPP := point.RegularPolygonPoint(r, w, h, 8)
+		WriteSVG(os.Stdout, w, h, paramPP, titlePP, textPP)
 		//WriteSVG(os.Stdout, "test", w, h, paramPos, titlePos, paramNames, titlePP)
 		break
 	}
@@ -98,7 +100,7 @@ func RegularPolygon(r, w, h float64, polygonCount int) (paramPos PolygonPosition
 	return
 }
 
-func WriteSVG2(wr io.Writer, w, h float64, paramPP, titlePP point.PolygonPoint) {
+func WriteSVG(wr io.Writer, w, h float64, paramPP, titlePP, textPP point.PolygonPoint) {
 	var (
 		wi = int(w)
 		hi = int(h)
@@ -124,35 +126,12 @@ func WriteSVG2(wr io.Writer, w, h float64, paramPP, titlePP point.PolygonPoint) 
 		p := point.RegularPolygonPoint(r, w, h, len(paramNames))
 		canvas.Polygon(p.Xs().Int(), p.Ys().Int(), "fill:none; stroke:#BDBDBD;")
 	}
-	canvas.End()
-}
-func WriteSVG(wr io.Writer, title string, w, h int, paramPos, titlePos PolygonPosition, paramNames []string, titlePP point.PolygonPoint) {
-	canvas := svg.New(wr)
-	canvas.Start(w, h)
-	canvas.Circle(w/2, h/2, 100)
-	// 外枠の描画
-	canvas.Polygon(titlePP.Xs().Int(), titlePP.Ys().Int(), "fill:#FAFAFA; stroke:#BDBDBD; ")
-	// canvas.Polygon(titlePos.X, titlePos.Y, "fill:#FAFAFA; stroke:#BDBDBD; ")
-	// パラメータ線の描画
-	canvas.Polygon(paramPos.X, paramPos.Y, "fill:#BBD9E7; stroke:#91C0DA; stroke-width: 3px;")
-	// 中央線の描画
-	for i, x := range titlePos.X {
-		cx, cy := w/2, h/2
-		y := titlePos.Y[i]
-		canvas.Line(cx, cy, x, y, "fill:#FAFAFA; stroke:#BDBDBD; ")
-	}
-	// 等間隔基準線の描画
-	for i := 0; i < 5; i++ {
-		r := w / 2 * i / 5
-		p := RegularPolygon(float64(r), float64(w), float64(h), len(paramNames))
-		canvas.Polygon(p.X, p.Y, "fill:none; stroke:#BDBDBD;")
-	}
-	// canvas.Text(w/2, h/2, title, "text-anchor:middle; font-size:30px; fill:white;")
-	for i := 0; i < len(titlePos.X); i++ {
-		x := titlePos.X[i]
-		y := titlePos.Y[i]
-		t := paramNames[i]
-		canvas.Text(x, y, t, "text-anchor:middle; font-size:30px; fill:black;")
+	for i, v := range textPP.Points {
+		var (
+			x, y = int(v.X), int(v.Y)
+			n    = paramNames[i]
+		)
+		canvas.Text(x, y, n, "text-anchor:middle; font-size:30px; fill:black;")
 	}
 	canvas.End()
 }
