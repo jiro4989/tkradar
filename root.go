fe113533eb2ca4de89c0a787cea88f5cfdbd1b84
jiro4989 <jiroron666@gmail.com>
2019-01-04 07:08:27 +0900

    :recycle:変数名を変更

--- root.go
+++ root.go
@@ -87,16 +87,16 @@ var RootCommand = &cobra.Command{
 					continue
 				}
 				paramPP := point.FetchPolygonPoint(c.FetchLastParams(), ParamMaxes, paramR, cp)
-				titlePP := point.RegularPolygonPoint(paramR, w, h, 8, cp)
+				framePP := point.RegularPolygonPoint(paramR, w, h, 8, cp)
 				textPP := point.RegularPolygonPoint(textR, w, h, 8, cp)
-				WriteSVG(os.Stdout, w, h, cp, paramPP, titlePP, textPP)
+				WriteSVG(os.Stdout, w, h, cp, paramPP, framePP, textPP)
 				break
 			}
 		}
 	},
 }
 
-func WriteSVG(wr io.Writer, w, h float64, cp point.Point, paramPP, titlePP, textPP point.PolygonPoint) {
+func WriteSVG(wr io.Writer, w, h float64, cp point.Point, paramPP, framePP, textPP point.PolygonPoint) {
 	var (
 		wi    = int(w)
 		hi    = int(h)
@@ -105,24 +105,29 @@ func WriteSVG(wr io.Writer, w, h float64, cp point.Point, paramPP, titlePP, text
 	canvas := svg.New(wr)
 	canvas.Start(wi, hi)
 	canvas.Circle(wi/2, hi/2, 100)
+
 	// 外枠の描画
-	canvas.Polygon(titlePP.Xs().Int(), titlePP.Ys().Int(), "fill:#FAFAFA; stroke:#BDBDBD; ")
+	canvas.Polygon(framePP.Xs().Int(), framePP.Ys().Int(), "fill:#FAFAFA; stroke:#BDBDBD; ")
+
 	// パラメータ線の描画
 	canvas.Polygon(paramPP.Rotate(angle, cp).Xs().Int(), paramPP.Rotate(angle, cp).Ys().Int(), "fill:#BBD9E7; stroke:#91C0DA; stroke-width: 3px;")
+
 	// 中央線の描画
-	for _, p := range titlePP.Points {
+	for _, p := range framePP.Points {
 		var (
 			x, y   = int(p.X), int(p.Y)
 			cx, cy = wi / 2, hi / 2
 		)
 		canvas.Line(cx, cy, x, y, "fill:#FAFAFA; stroke:#BDBDBD; ")
 	}
+
 	// 等間隔基準線の描画
 	for i := 0; i < 5; i++ {
 		r := (w/2 - 25) * float64(i) / 5
 		p := point.RegularPolygonPoint(r, w, h, len(paramNames), cp)
 		canvas.Polygon(p.Xs().Int(), p.Ys().Int(), "fill:none; stroke:#BDBDBD;")
 	}
+
 	// テキストの描画
 	for i, v := range textPP.Rotate(angle, cp).Points {
 		var (
@@ -131,5 +136,6 @@ func WriteSVG(wr io.Writer, w, h float64, cp point.Point, paramPP, titlePP, text
 		)
 		canvas.Text(x, y, n, "text-anchor:middle; font-size:30px; fill:black;")
 	}
+
 	canvas.End()
 }
